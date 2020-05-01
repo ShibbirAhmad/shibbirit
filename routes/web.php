622a504122f1c929a43d72ec83b23693a1314090
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-05-01 23:41:55 +0600

    Admin Dashboard layout implemented

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,16 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('site.index');
+})->name('home');
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
 //this group for admin 
 Route::group(['as' => 'admin.','prefix' => 'admin' , 'namespace' => 'Admin', 'middleware'=> ['auth','admin']], function () {
@@ -26,6 +27,7 @@ Route::group(['as' => 'admin.','prefix' => 'admin' , 'namespace' => 'Admin', 'mi
 
 
 });
+
 
 //this route group for author
 Route::group(['as' => 'author.','prefix' => 'author' , 'namespace' => 'Author', 'middleware'=> ['auth','author']], function () {
