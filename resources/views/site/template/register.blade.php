ed5f9e7cc954e7fed8b2cb40958584f4ac61e9a2
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-05-01 23:32:50 +0600

    frontend site blade template is ready

---
+++ resources/views/site/template/register.blade.php
@@ -0,0 +1,99 @@
+@extends('site.layout.master')
+
+@section('title','login')
+
+@push('css')
+    
+@endpush
+
+@section('slider')
+
+              
+          <div style="overflow:hidden;text-align:center" class="slider">
+               <h2 style="color:#fff;margin-top:120px;">REGISTER HERE</h2>
+          </div>
+
+@endsection
+
+
+@section('content')
+    
+			<div class="row">
+				<div class="col-lg-2 col-md-0"></div>
+				<div class="col-lg-8 col-md-12">
+					<div class="post-wrapper">
+
+                                            <form method="POST" action="{{ route('register') }}">
+                                                @csrf
+                        
+                                                <div class="form-group row">
+                                                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        
+                                                    <div class="col-md-6">
+                                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        
+                                                        @error('name')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                        
+                                                <div class="form-group row">
+                                                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        
+                                                    <div class="col-md-6">
+                                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        
+                                                        @error('email')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                        
+                                                <div class="form-group row">
+                                                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        
+                                                    <div class="col-md-6">
+                                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                        
+                                                        @error('password')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                        
+                                                <div class="form-group row">
+                                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        
+                                                    <div class="col-md-6">
+                                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                                    </div>
+                                                </div>
+                        
+                                                <div class="form-group row mb-0">
+                                                    <div class="col-md-6 offset-md-4">
+                                                        <button type="submit" class="btn btn-primary">
+                                                            {{ __('Register') }}
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                       
+
+					</div><!-- post-wrapper -->
+				</div><!-- col-sm-8 col-sm-offset-2 -->
+			</div><!-- row -->
+
+@endsection
+
+
+
+@push('js')
+    
+@endpush
