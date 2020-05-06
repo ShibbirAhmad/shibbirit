86e2a2da774a870349a0321c1a9ba46be3386078
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-05-06 14:31:41 +0600

    view all favourtie post  in author remove favourite if needed

---
+++ app/Http/Controllers/Admin/FavouriteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class FavouriteController extends Controller
+{
+    public function index(){
+         
+          $posts=Auth::user()->favourite_posts;
+
+          return view('admin.favourite',compact(['posts']) );
+    }
+    
+    public function removeFavourite($postId){
+
+          
+         $user=Auth::user();
+         $isFavourite=$user->favourite_posts()->where('post_id',$postId)->count();
+
+         if ($isFavourite == 1) {
+            
+            $user->favourite_posts()->detach($postId);
+            return redirect()->back()->with('warning','this post remove from your favourite list');
+         }
+
+
+    }
+
+
+}
---
+++ app/Http/Controllers/Author/FavouriteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Author;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FavouriteController extends Controller
+{
+    //
+}
